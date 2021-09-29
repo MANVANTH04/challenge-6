940617cad2a6878c5aae20cf34472742ea68e3d4
manvanth04 <75923745+MANVANTH04@users.noreply.github.com>
2021-09-29 11:45:55 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+// const shareIcon = document.querySelector('.share-icon');
+// const toolTip = document.querySelector('.tooltip');
+        
+    // shareIcon.addEventListener('click', () => { // Adds event when .share-icon is cliked
+        // toolTip.classList.toggle('hide'); // Show/hide .tooltip
+        // shareIcon.classList.toggle('fill'); // Changes color of the share icon SVG
+        // shareIcon.classList.toggle('icon-bg'); //Changes background color of the .share-icon
+        // }
+    // )
